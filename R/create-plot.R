de137869fc79c9d4889fb2e118be2bd73a2e042a
Sarah Lucas <Sarah.Lucas@cantab.net>
2022-06-23 14:38:53 +0100

    added facet for wind direction

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,8 +15,9 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour="red")+
-  theme_minimal()
+  geom_point(colour="red") +
+  theme_minimal() + 
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
